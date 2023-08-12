c479f19d7fb959c48c695bb2d3df059dc44524ed
Arvin Delavari <arvin7807@gmail.com>
2023-08-12 18:26:04 +0330

    Fix Memory Interface

--- Memory_Interface.v
+++ Memory_Interface.v
@@ -1,30 +1,44 @@
 module Memory_Interface 
 (
-  input wire clk,
-  input wire reset,
-  input wire enable,
-  input wire read_enable,
-  input wire write_enable,
-  input wire [31 : 0] address,
-  input wire [31 : 0] data_in,
-  output reg [31 : 0] data_out
+  input enable,
+  input memory_state,
+
+  input [3  : 0] frame_mask,
+  input [31 : 0] address,
+  inout [31 : 0] data
 );
 
-  reg [31 : 0] memory [0 : 255];
+  reg [31 : 0] data_in;
+  reg [31 : 0] data_out;
 
-  always @(posedge clk or posedge reset) begin
+  reg [7 : 0] memory [0 : 1023];
+
+  always @(*) begin
     
-    if (reset) begin
-      data_out <= 32'b0; // Reset data output to 0
-    end 
-    
-    else if (enable) begin
+    if (enable) begin
       if (address <= 255) begin // Check if address is within memory range
-        if (write_enable) begin 
-          memory[address] <= data_in;  // Write operation
+
+        if (memory_state) begin         // Write operation
+
+          case (frame_mask)
+            // Store Byte 
+            4'b0001 : memory[address + 3] = data;
+            4'b0010 : memory[address + 2] = data;
+            4'b0100 : memory[address + 1] = data;
+            4'b1000 : memory[address + 0] = data;
+            // Store Half-word 
+            4'b0011 : begin
+              memory[address + 2] = data;
+            end
+            4'b1100 : memory[address + 0] = data;
+          endcase
+
+          
         end 
-        else if (read_enable) begin 
-          data_out <= memory[address]; // Read operation
+        else if (!memory_state) begin   // Read operation
+
+
+
         end
       end
     end
