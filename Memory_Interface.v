3c0a17daf2668d9d0fa6baf98d4cfa5b5fcede94
Arvin Delavari <arvin7807@gmail.com>
2023-08-12 14:55:44 +0330

    Fix memory interface module

--- Memory_Interface.v
+++ Memory_Interface.v
@@ -6,8 +6,8 @@ module Memory_Interface
   input wire read_enable,
   input wire write_enable,
   input wire [31 : 0] address,
-  input wire [31 : 0] dataIn,
-  output reg [31 : 0] dataOut
+  input wire [31 : 0] data_in,
+  output reg [31 : 0] data_out
 );
 
   reg [31 : 0] memory [0 : 255];
@@ -15,16 +15,16 @@ module Memory_Interface
   always @(posedge clk or posedge reset) begin
     
     if (reset) begin
-      dataOut <= 32'b0; // Reset data output to 0
+      data_out <= 32'b0; // Reset data output to 0
     end 
     
     else if (enable) begin
       if (address <= 255) begin // Check if address is within memory range
         if (write_enable) begin 
-          memory[address] <= dataIn;  // Write operation
+          memory[address] <= data_in;  // Write operation
         end 
         else if (read_enable) begin 
-          dataOut <= memory[address]; // Read operation
+          data_out <= memory[address]; // Read operation
         end
       end
     end
