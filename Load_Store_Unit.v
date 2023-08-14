6476c5c81a29027c0d2848e0ae60a65acf6b5d10
Arvin Delavari <arvin7807@gmail.com>
2023-08-14 17:16:16 +0330

    Update Load Store Unit with new Memory Interface

--- Load_Store_Unit.v
+++ Load_Store_Unit.v
@@ -3,21 +3,48 @@
 module Load_Store_Unit
 (     
     input CLK,
-    input [6 : 0] opcode,
-    input [2 : 0] funct3,
+    input enable,
+    
+    input  [6 : 0] opcode,
+    input  [2 : 0] funct3,
 
-    input [31 : 0] bus_rs1,   // For Address Generation
-    input [31 : 0] immediate, // For Address Generation
-
-    input  [31 : 0] Store_Data,
-    output [31 : 0] Load_Data
+    input  [31 : 0] address,
+    input  [31 : 0] store_data,
+    output [31 : 0] load_data
 );
-    wire enable;
-    wire read_enable;  // Load Instruction
-    wire write_enable; // Store Instruction
+    wire   memory_state;
+    wire   memory_done;
+    wire   [3  : 0] frame_mask;
+    wire   [31 : 0] data;
+    
 
-    // -------------- INSTRUCTION DECODING MUST BE ADDED --------------
+    always @(*) begin
 
-    // Memory_Interface data_memory (CLK, 1'b0, enable, read_enable, write_enable, address, Load_Data, Store_Data);
+        casex ({funct3, opcode})
+            // Load Instructions
+            10'b000_0000011 : 
+            10'b001_0000011 :
+            10'b010_0000011 :
+            10'b100_0000011 : 
+            10'b101_0000011 : 
+            // Store Instructions
+            10'b000_0100011 : 
+            10'b001_0100011 :
+            10'b010_0100011 :
+            default: 
+        endcase
+        
+    end
+
+    Memory_Interface data_memory 
+    (
+        .CLK(CLK),
+        .enable(enable), 
+        .memory_state(memory_state),
+        .frame_mask(frame_mask),
+        .address(address), 
+        .data(data), 
+        .memory_done(memory_done)
+    );
 
 endmodule
