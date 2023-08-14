044da15b66b7c12f0ff90a9060141d3ab56d1a37
Arvin Delavari <arvin7807@gmail.com>
2023-08-14 12:26:26 +0330

    Update Jump Branch Unit

--- Branch_Unit.v
+++ Branch_Unit.v
@@ -1,3 +1,10 @@
+`define I_TYPE 0
+`define B_TYPE 1
+`define S_TYPE 2
+`define U_TYPE 3
+`define J_TYPE 4
+`define R_TYPE 6
+
 `define BEQ  0
 `define BNE  1
 `define BLT  2
@@ -5,25 +12,29 @@
 `define BLTU 4
 `define BGEU 5
 
-module Branch_Unit 
+module Jump_Branch_Unit 
 (
     input [6 : 0] opcode,
     input [2 : 0] funct3,
     input [6 : 0] funct7,
+    input [2 : 0] instruction_type,
 
     input [31 : 0] bus_rs1,
     input [31 : 0] bus_rs2,
 
-    input branch_signal,     // From Instruction_Decoder
+    input branch_signal,          // From Instruction_Decoder
       
-    output reg branch_enable // Goes to Fetch_Unit
+    output jump_branch_enable     // Goes to Fetch_Unit
 );
 
     reg [2 : 0] branch_type;
 
+    reg branch_enable;
+    reg jump_enable;
+
     always @(*) begin
 
-            if (branch_signal) begin                  // B-TYPE Instructions
+            if (branch_signal) begin                // B-TYPE Instructions
 
                 casex ({funct7, funct3, opcode})
                 17'bxxxxxxx_000_1100011 : begin     // BEQ
@@ -56,12 +67,17 @@ module Branch_Unit
                     if (bus_rs1 >= bus_rs2)
                             branch_enable = 1'b1;
                 end
-                default: branch_enable = 1'b0;
+                default:    branch_enable = 1'b0;
                 endcase
 
             end else
                 branch_enable = 1'b0;
 
+            if (instruction_type == `J_TYPE)
+                jump_enable = 1'b1;
+
       end  
+
+      assign jump_branch_enable = jump_enable || branch_enable;
       
 endmodule
