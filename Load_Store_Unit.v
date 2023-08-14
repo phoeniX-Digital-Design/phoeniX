20a794048b66a2428eb4a82834a03641becfef49
Arvin Delavari <arvin7807@gmail.com>
2023-08-14 17:49:15 +0330

    LSU state and mask generation

--- Load_Store_Unit.v
+++ Load_Store_Unit.v
@@ -5,35 +5,68 @@ module Load_Store_Unit
     input CLK,
     input enable,
     
-    input  [6 : 0] opcode,
-    input  [2 : 0] funct3,
+    input  [6 : 0] opcode,      // Load/Store function
+    input  [2 : 0] funct3,      // Load/Store function
 
-    input  [31 : 0] address,
-    input  [31 : 0] store_data,
+    input  [31 : 0] address,    // Generated in Address Generator module
+    input  [31 : 0] store_data, // Connected to Register Source 2
     output [31 : 0] load_data
 );
-    wire   memory_state;
+
+    reg   memory_state;
+    reg   [3  : 0] frame_mask;
+
     wire   memory_done;
-    wire   [3  : 0] frame_mask;
     wire   [31 : 0] data;
     
-
-    always @(*) begin
-
+    always @(*) 
+    begin
         casex ({funct3, opcode})
             // Load Instructions
-            10'b000_0000011 : 
-            10'b001_0000011 :
-            10'b010_0000011 :
-            10'b100_0000011 : 
-            10'b101_0000011 : 
+
+            // LB and LBU
+            10'bX00_0000011 : {memory_state, frame_mask} = 
+            {   data_memory.READ, 
+            {                   
+                ~address[1] & ~address[0], 
+                ~address[1] &  address[0], 
+                 address[1] & ~address[0], 
+                 address[1] &  address[0]
+            }
+            }; 
+            // LH and LHU
+            10'bX01_0000011 : {memory_state, frame_mask} = 
+            {   data_memory.READ,
+            {                   
+                {2{~address[1]}}, {2{address[1]}}
+            }
+            }; 
+            // LW
+            10'b010_0000011 : {memory_state, frame_mask} = {data_memory.READ, 4'b1111}; 
+            
             // Store Instructions
-            10'b000_0100011 : 
-            10'b001_0100011 :
-            10'b010_0100011 :
-            default: 
-        endcase
-        
+
+            // SB
+            10'b000_0100011 : {memory_state, frame_mask} = 
+            {   data_memory.WRITE, 
+            {                   
+                ~address[1] & ~address[0], 
+                ~address[1] &  address[0], 
+                 address[1] & ~address[0], 
+                 address[1] &  address[0]
+            }
+            }; 
+            // SH
+            10'b001_0100011 : {memory_state, frame_mask} = 
+            {   data_memory.WRITE,
+            {                   
+                {2{~address[1]}}, {2{address[1]}}
+            }
+            }; 
+            // SW
+            10'b010_0100011 : {memory_state, frame_mask} = {data_memory.WRITE, 4'b1111};
+            default: {memory_state, frame_mask} = 5'bz;
+        endcase    
     end
 
     Memory_Interface data_memory 
