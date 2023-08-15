81686fe18967ac2dc1986c7f505b36a2224b5545
Arvin Delavari <arvin7807@gmail.com>
2023-08-15 09:48:42 +0330

    Define Control Unit inputs

--- Control_Unit.v
+++ Control_Unit.v
@@ -2,10 +2,15 @@ module Control_Unit
 (
       input [6 : 0] opcode,
       input [2 : 0] funct3,
-      input [6 : 0] funct7
+      input [6 : 0] funct7,
+      input [2 : 0] instruction_type,
+
+      input [4 : 0] read_index_1,
+      input [4 : 0] read_index_2,
+      input [4 : 0] write_index
 
 
-      // Outputs :)))
+      // inputs :)))
 );
       
 endmodule
