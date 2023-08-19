12f2e2eb80edf7a3e37d0c02e2b0b8e330942a4f
Arvin Delavari <arvin7807@gmail.com>
2023-08-19 10:51:45 +0330

    Edit writeback mux

--- Writeback_Mux.v
+++ Writeback_Mux.v
@@ -1,12 +1,12 @@
 module Writeback_Mux 
 (
-      input  writeback_output_select,     // Signal from Control Unit
-      input  [31 : 0] load_data,          // LSU Load instruction output
-      input  [31 : 0] execution_output,   // Execution stage latched output
+    input  writeback_output_select,     // Signal from Control Unit
+    input  [31 : 0] load_data,          // LSU Load instruction output
+    input  [31 : 0] execution_output,   // Execution stage latched output
 
-      output [31 : 0] writeback_output
+    output [31 : 0] writeback_output
 );
 
-      assign writeback_output = writeback_output_select ? load_data : execution_output;
+    assign writeback_output = writeback_output_select ? load_data : execution_output;
       
 endmodule
