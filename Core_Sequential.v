20d47ea67dce08684066b0a7b18e9840285db74f
Arvin Delavari <arvin7807@gmail.com>
2023-08-19 14:49:07 +0330

    Created sequential core file

---
+++ Core_Sequential.v
@@ -0,0 +1,22 @@
+`include "Fetch_Unit.v"
+
+`include "Instruction_Decoder.v"
+`include "Immediate_Generator.v"
+
+`include "Bypass_Mux.v"
+
+`include "Arithmetic_Logic_Unit.v"
+`include "Jump_Branch_Unit.v"
+`include "Address_Generator.v"
+
+`include "Load_Store_Unit.v"
+`include "Writeback_Mux.v"
+
+`include "Control_Unit.v"
+`include "Register_File.v"
+
+module Core_Sequential;
+
+    
+    
+endmodule
