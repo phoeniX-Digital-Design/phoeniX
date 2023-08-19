f50abc5c476bc587583e1722df7eed7504c4f8f0
Arvin Delavari <arvin7807@gmail.com>
2023-08-19 11:35:18 +0330

    Add LUI support to writeback multiplexer

--- Writeback_Mux.v
+++ Writeback_Mux.v
@@ -1,12 +1,21 @@
 module Writeback_Mux 
 (
-    input  writeback_output_select,     // Signal from Control Unit
-    input  [31 : 0] load_data,          // LSU Load instruction output
-    input  [31 : 0] execution_output,   // Execution stage latched output
+    input  [1  : 0] writeback_output_select,        // Signal from Control Unit
+    input  [31 : 0] immediate,                      // Immediate data
+    input  [31 : 0] load_data,                      // LSU Load instruction output
+    input  [31 : 0] execution_output,               // Execution stage latched output
 
-    output [31 : 0] writeback_output
+    output reg [31 : 0] writeback_output
 );
 
-    assign writeback_output = writeback_output_select ? load_data : execution_output;
+    always @(*) begin
+        
+        case (writeback_output_select)
+            2'b00 : writeback_output = immediate; 
+            2'b01 : writeback_output = load_data;
+            2'b10 : writeback_output = execution_output;
+        endcase
+
+    end
       
 endmodule
