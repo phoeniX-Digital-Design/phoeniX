27cdfb33d6965e1434117d7291eb94561623d11e
Faraz <farazghoreishy@gmail.com>
2023-08-20 12:16:20 +0330

    parametrize register module and add reset value

--- Register.v
+++ Register.v
@@ -1,15 +1,22 @@
-module Register
+module Register #(
+    parameter WIDTH = 32
+)
 (
     input CLK,
+    input reset,
     input enable,
-    input [31 : 0] register_input,
-    
+    input   [WIDTH : 0] RESET_ADDRESS,
+
+    input   [WIDTH : 0] register_input,
     output reg [31 : 0] register_output
 );
-    always @(negedge CLK)
+    always @(posedge CLK)
     begin
-        if(enable)
+        if (reset)
+            register_output <= RESET_ADDRESS;
+        else if(enable)
             register_output <= register_input;
+        else
+            register_output <=  {WIDTH{1'bz}};
     end
-    
 endmodule
