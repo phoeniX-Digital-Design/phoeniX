7f1ada6239d343dbadcbcb3a5fd546876e76be58
Arvin Delavari <arvin7807@gmail.com>
2023-08-20 19:08:41 +0330

    Edit sum 1 to 100 test code

--- Test_RV32I_sum1to100.s
+++ Test_RV32I_sum1to100.s
@@ -1,13 +1,13 @@
 add x10, x0, x0
 
 add x14, x10, x0 
-addi x12, x10, 1010
+addi x12, x10, 10
 add x13, x10, x0 
 
-add x14, x13, x14
+LOOP : add x14, x13, x14
 addi x13, x13, 1
-blt x13, x12, 1111111111000
-add x10, x14, r0 
+blt x13, x12, LOOP
+add x10, x14, x0 
    
-sw x0, x10, 100
-lw x17, x0, 100
+sw x0, 100(x10)
+lw x17, 100(x0)
