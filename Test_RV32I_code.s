9da67bbd6954cde35f97e304a82a9a6fdb712dce
Faraz <farazghoreishy@gmail.com>
2023-08-20 19:08:23 +0330

    updating first test code

--- Test_RV32I_code.s
+++ Test_RV32I_code.s
@@ -1,4 +1,10 @@
-addi x2, x0, 1
-addi x3, x0, 2
-add  x4, x2, x3
-lw   x5, 0(x4) 
+        add x1, x0, x0
+        add x7, x0, x0
+LOOP:   addi x2, x0, 1
+        addi x3, x0, 2
+        addi x0, x0, 0
+        addi x0, x0, 0
+        add  x4, x2, x3
+        lw   x5, 0(x4) 
+        beq x2, x3, LOOP # if x2 =x3t1 then target
+        
