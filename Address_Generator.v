06072e75c6235eac4e43bd7d3926f063f6af69bc
Faraz <farazghoreishy@gmail.com>
2023-08-20 18:09:05 +0330

    adding control signals to address generator

--- Address_Generator.v
+++ Address_Generator.v
@@ -10,16 +10,28 @@
 
 module Address_Generator
 (
-    input  address_type,
-    input  [31 : 0] rs1,            // to be connected to bus_rs1
-    input  [31 : 0] PC,
-    input  [31 : 0] immediate,
+    input [6 : 0] opcode, 
+    input [31 : 0] rs1,            // to be connected to bus_rs1
+    input [31 : 0] PC,
+    input [31 : 0] immediate,
 
     output [31 : 0] address
 );
-    wire [31 : 0] value;
+    reg  address_type;
+    always @(*) 
+    begin
+        // Address Type evaluation (for Address Generator module)
+        case (opcode)
+            7'b0100011: address_type = 1'b1;    //  Store  -> bus_rs1 + immediate
+            7'b0000011: address_type = 1'b1;    //  Load   -> bus_rs1 + immediate
+            7'b1101111: address_type = 1'b0;    //  JAL    ->    PC   + immediate
+            7'b1100111: address_type = 1'b0;    //  JALR   ->    PC   + immediate
+            7'b1100011: address_type = 1'b0;    //  Branch ->    PC   + immediate
+            default: address_type = 1'bz;
+        endcase 
+    end
 
-    assign value = address_type ? rs1 : PC;
+    wire [31 : 0] value = address_type ? rs1 : PC;
     assign address = value + immediate;
     
 endmodule
