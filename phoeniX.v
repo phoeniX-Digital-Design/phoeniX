69a4bd560ecfccb36b2cb6592ab7bd2d64fcd7bc
Faraz <farazghoreishy@gmail.com>
2023-08-20 16:28:30 +0330

    phoeniX pipelined IF & ID & EX

--- phoeniX.v
+++ phoeniX.v
@@ -1,6 +1,5 @@
 `include "Register.v"
 `include "Fetch_Unit.v"
-`include "Control_Unit.v"
 
 `include "Instruction_Decoder.v"
 `include "Immediate_Generator.v"
@@ -50,7 +49,6 @@ module phoeniX
     // ------------------------
     // Program Counter Register 
     // ------------------------
-
     always @(posedge CLK)
     begin
         if (reset)
@@ -58,16 +56,6 @@ module phoeniX
         else
             PC_fetch_reg <= next_PC_wire; 
     end
-    // Register program_counter 
-    // (
-    //     .CLK(CLK),
-    //     .reset(reset),
-    //     .enable(1'b1),               // TBD : to be changed to fetch control state with control     
-    //     .RESET_VALUE(RESET_ADDRESS),
-
-    //     .register_input(next_PC_reg),
-    //     .register_output(PC_fetch_wire)
-    // );
     
     // --------------------------------------
     // Register Declarations for Decode Stage
@@ -95,6 +83,9 @@ module phoeniX
     wire [4 : 0] read_index_2_decode_wire;
     wire [4 : 0] write_index_decode_wire;
     wire [31 : 0] immediate_decode_wire;
+    wire read_enable_1_decode_wire;
+    wire read_enable_2_decode_wire;
+    wire write_enable_decode_wire;
 
     // ---------------------------------
     // Instruction Decoder Instantiation
@@ -108,7 +99,10 @@ module phoeniX
         .funct7(funct7_decode_wire),
         .read_index_1(read_index_1_decode_wire),
         .read_index_2(read_index_2_decode_wire),
-        .write_index(write_index_decode_wire)
+        .write_index(write_index_decode_wire),
+        .read_enable_1(read_enable_1_decode_wire),
+        .read_enable_2(read_enable_2_decode_wire),
+        .write_enable(write_enable_decode_wire)
     );
 
     // ---------------------------------
@@ -121,4 +115,164 @@ module phoeniX
         .immediate(immediate_decode_wire)
     );
 
+    // -----------------------------------------------
+    // Wire Declaration for Reading From Register File
+    // ----------------------------------------------- 
+    wire [31 : 0] RF_source_1;
+    wire [31 : 0] RF_source_2;
+
+    // -----------------------------------------------
+    // Wire Declaration for inputs to source bus 1 & 2
+    // ----------------------------------------------- 
+    wire [31 : 0] bus_rs1_decode_wire;
+    wire [31 : 0] bus_rs2_decode_wire;
+
+    // ----------------------------------------------------------------------------------------
+    // assign inputs to source bus 1 & 2  --> TBD: to be selected between RF source and FW data
+    // ----------------------------------------------------------------------------------------
+    assign bus_rs1_decode_wire = RF_source_1;
+    assign bus_rs2_decode_wire = RF_source_2;
+    
+    // ----------------------------------
+    // Reg Declarations for Execute Stage
+    // ----------------------------------
+    reg [31 : 0] PC_execute_reg;
+
+    reg [6 : 0] opcode_execute_reg;
+    reg [2 : 0] funct3_execute_reg;
+    reg [6 : 0] funct7_execute_reg;
+
+    reg [31 : 0] immediate_execute_reg;
+    reg [2 : 0] instruction_type_execute_reg;
+    reg [4 : 0] write_index_execute_reg;
+    reg write_enable_execute_reg;
+    
+    reg [31 : 0] bus_rs1;
+    reg [31 : 0] bus_rs2;
+
+    ////////////////////////////////////////
+    //    DECODE TO EXECUTE TRANSITION    //
+    ////////////////////////////////////////
+    always @(posedge CLK) 
+    begin
+        PC_execute_reg <= PC_decode_reg;
+
+        opcode_execute_reg <= opcode_decode_wire;
+        funct3_execute_reg <= funct3_decode_wire;
+        funct7_execute_reg <= funct7_decode_wire;
+
+        immediate_execute_reg <= immediate_decode_wire; 
+        instruction_type_execute_reg <= instruction_type_decode_wire;
+        write_index_execute_reg <= write_index_decode_wire;
+        write_enable_execute_reg <= write_enable_decode_wire;    
+    end
+
+    // ------------------------------------
+    // Wire Declaration for Execution Units
+    // ------------------------------------
+    wire [31 : 0] alu_output_execute_wire;
+
+    // -----------------------------------
+    // Arithmetic Logic Unit Instantiation
+    // -----------------------------------
+    Arithmetic_Logic_Unit arithmetic_logic_unit
+    (
+        .opcode(opcode_execute_reg),
+        .funct3(funct3_execute_reg),
+        .funct7(funct7_execute_reg),
+
+        .PC(PC_execute_reg),
+        .rs1(bus_rs1),
+        .rs2(bus_rs2),
+        .immediate(immediate_execute_reg),
+        .alu_output(alu_output_execute_wire)
+    );
+
+
+    // --------------------------------
+    // Reg Declarations for Memory Stage
+    // --------------------------------
+    reg [31 : 0] PC_memory_reg;
+
+    reg [6 : 0] opcode_memory_reg;
+    reg [2 : 0] funct3_memory_reg;
+    reg [6 : 0] funct7_memory_reg;
+
+    reg [31 : 0] immediate_memory_reg;
+    reg [2 : 0] instruction_type_memory_reg;
+    reg [4 : 0] write_index_memory_reg;
+    reg write_enable_memory_reg;
+
+    ////////////////////////////////////////
+    //    EXECUTE TO MEMORY TRANSITION    //
+    ////////////////////////////////////////
+    always @(posedge CLK) 
+    begin
+        PC_memory_reg <= PC_execute_reg;
+
+        opcode_memory_reg <= opcode_execute_reg;
+        funct3_memory_reg <= funct3_execute_reg;
+        funct7_memory_reg <= funct7_execute_reg;
+        
+        immediate_memory_reg <= immediate_execute_reg;
+        instruction_type_memory_reg <= instruction_type_execute_reg;
+        write_index_memory_reg <= write_index_execute_reg;
+        write_enable_memory_reg <= write_enable_execute_reg;    
+    end
+
+
+    // --------------------------------
+    // Reg Declarations for Write-Back Stage
+    // --------------------------------
+    reg [31 : 0] PC_writeback_reg;
+
+    reg [6 : 0] opcode_writeback_reg;
+    reg [2 : 0] funct3_writeback_reg;
+    reg [6 : 0] funct7_writeback_reg;
+
+    reg [31 : 0] write_data_writeback_reg;
+    reg [4 : 0] write_index_writeback_reg;
+    reg write_enable_writeback_reg;
+
+    ////////////////////////////////////////
+    //   MEMORY TO WRITEBACK TRANSITION   //
+    ////////////////////////////////////////
+    always @(posedge CLK) 
+    begin
+        PC_writeback_reg <= PC_memory_reg;
+
+        opcode_writeback_reg <= opcode_memory_reg;
+        funct3_writeback_reg <= funct3_memory_reg;
+        funct7_writeback_reg <= funct7_memory_reg;
+
+        write_index_writeback_reg <= write_index_memory_reg;
+        write_enable_writeback_reg <= write_enable_memory_reg;    
+    end
+    
+
+    ////////////////////////////////////////
+    //    Register File Instantiation     //
+    ////////////////////////////////////////
+    Register_File 
+    #(
+        .WIDTH(32),
+        .DEPTH(5)
+    )
+    register_file
+    (
+        .CLK(CLK),
+        .reset(reset),
+
+        .read_enable_1(1'b0 & read_enable_1),
+        .read_enable_2(1'b0 & read_enable_2),
+        .write_enable(1'b0 & write_enable_writeback_reg),
+
+        .read_index_1(read_index_1_decode_wire),
+        .read_index_2(read_index_2_decode_wire),
+        .write_index(write_index_decode_wire),
+
+        .write_data(write_data_writeback_reg),
+        .read_data_1(RF_source_1),
+        .read_data_2(RF_source_2)
+    );
 endmodule
