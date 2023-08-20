ac08ac842905259f9b836737586be7eebe1d9db2
Faraz <farazghoreishy@gmail.com>
2023-08-20 10:06:15 +0330

    Minor change

--- Register_File.v
+++ Register_File.v
@@ -25,7 +25,7 @@ module Register_File
     integer i;    	
     initial
     begin
-        for (i = 0 ; i < $pow(2, DEPTH) - 1; i = i + 1)
+        for (i = 0; i < $pow(2, DEPTH) - 1; i = i + 1)
             Registers[i] = {WIDTH{1'b0}};
     end
 	
