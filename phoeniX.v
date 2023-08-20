d06f33ac9c4b2ff3fad1bf19c65135dbece9fac0
Faraz <farazghoreishy@gmail.com>
2023-08-20 19:47:14 +0330

    phoeniX piplined testing IF & ID & EX & WB

--- phoeniX.v
+++ phoeniX.v
@@ -40,8 +40,8 @@ module phoeniX
         .CLK(CLK_MEM),
         .enable(!reset),              // TBD : to be changed to fetch control state with control
         .PC(PC_fetch_reg),
-        .address(32'bz),
-        .jump_branch_enable(1'b0),
+        .address(address_memory_reg),
+        .jump_branch_enable(jump_branch_enable_memory_reg),
         .next_PC(next_PC_wire),
         .fetched_instruction(instruction_fetch_wire)    
     );
@@ -164,13 +164,18 @@ module phoeniX
         immediate_execute_reg <= immediate_decode_wire; 
         instruction_type_execute_reg <= instruction_type_decode_wire;
         write_index_execute_reg <= write_index_decode_wire;
-        write_enable_execute_reg <= write_enable_decode_wire;    
+        write_enable_execute_reg <= write_enable_decode_wire;  
+
+        bus_rs1 <= bus_rs1_decode_wire;
+        bus_rs2 <= bus_rs2_decode_wire;
     end
 
     // ------------------------------------
     // Wire Declaration for Execution Units
     // ------------------------------------
     wire [31 : 0] alu_output_execute_wire;
+    wire [31 : 0] address_execute_wire;
+    wire jump_branch_enable_execute_wire;
 
     // -----------------------------------
     // Arithmetic Logic Unit Instantiation
@@ -188,6 +193,41 @@ module phoeniX
         .alu_output(alu_output_execute_wire)
     );
 
+    // ------------------------------------
+    // Address Generator Unit Instantiation
+    // ------------------------------------
+    Address_Generator address_generator
+    (
+        .opcode(opcode_execute_reg),
+        .rs1(bus_rs1),
+        .PC(PC_execute_reg),
+        .immediate(immediate_execute_reg),
+        .address(address_execute_wire)
+    );
+
+    // ------------------------------
+    // Jump Branch Unit Instantiation
+    // ------------------------------
+    Jump_Branch_Unit jump_branch_unit
+    (
+        .opcode(opcode_execute_reg),
+        .funct3(funct3_execute_reg),
+        .funct7(funct7_execute_reg),
+        .instruction_type(instruction_type_execute_reg),
+        .rs1(bus_rs1),
+        .rs2(bus_rs2),
+        .jump_branch_enable(jump_branch_enable_execute_wire)
+    );
+
+    // ----------------------------------------
+    // Wire Declaration for result of execution
+    // ----------------------------------------
+    wire [31 : 0] result_execute_wire;
+
+    // ----------------------------------------------------------
+    // assigning result to alu output or mul output or fpu output
+    // ----------------------------------------------------------
+    assign result_execute_wire = alu_output_execute_wire;
 
     // --------------------------------
     // Reg Declarations for Memory Stage
@@ -202,6 +242,12 @@ module phoeniX
     reg [2 : 0] instruction_type_memory_reg;
     reg [4 : 0] write_index_memory_reg;
     reg write_enable_memory_reg;
+
+    reg [31 : 0] address_memory_reg;
+    reg [31 : 0] bus_rs2_memory_reg;
+    reg [31 : 0] result_memory_reg;
+
+    reg jump_branch_enable_memory_reg;
 
     ////////////////////////////////////////
     //    EXECUTE TO MEMORY TRANSITION    //
@@ -218,21 +264,41 @@ module phoeniX
         instruction_type_memory_reg <= instruction_type_execute_reg;
         write_index_memory_reg <= write_index_execute_reg;
         write_enable_memory_reg <= write_enable_execute_reg;    
+
+        address_memory_reg <= address_execute_wire;
+        bus_rs2_memory_reg <= bus_rs2;
+        result_memory_reg <= result_execute_wire;
+
+        result_memory_reg <= result_execute_wire;
+
+        jump_branch_enable_memory_reg <= jump_branch_enable_execute_wire;
     end
 
+    // ---------------------------------------------
+    // Wire Declaration for write back out of memory
+    // ---------------------------------------------
+    wire [31 : 0] write_data_memory_wire;
 
-    // --------------------------------
+    // --------------------------------------------------------------
+    // assigning write-back data to result or immediateo or load data
+    // --------------------------------------------------------------
+    assign write_data_memory_wire = result_memory_reg;
+
+    // -------------------------------------
     // Reg Declarations for Write-Back Stage
-    // --------------------------------
+    // -------------------------------------
     reg [31 : 0] PC_writeback_reg;
 
     reg [6 : 0] opcode_writeback_reg;
     reg [2 : 0] funct3_writeback_reg;
     reg [6 : 0] funct7_writeback_reg;
 
-    reg [31 : 0] write_data_writeback_reg;
+    reg [31 : 0] immediate_writeback_reg;
+    reg [2 : 0] instruction_type_writeback_reg;
     reg [4 : 0] write_index_writeback_reg;
     reg write_enable_writeback_reg;
+
+    reg [31 : 0] write_data_writeback_reg;
 
     ////////////////////////////////////////
     //   MEMORY TO WRITEBACK TRANSITION   //
@@ -245,11 +311,14 @@ module phoeniX
         funct3_writeback_reg <= funct3_memory_reg;
         funct7_writeback_reg <= funct7_memory_reg;
 
+        immediate_writeback_reg <= immediate_memory_reg;
+        instruction_type_writeback_reg <= instruction_type_memory_reg;
         write_index_writeback_reg <= write_index_memory_reg;
-        write_enable_writeback_reg <= write_enable_memory_reg;    
+        write_enable_writeback_reg <= write_enable_memory_reg; 
+
+        write_data_writeback_reg <= write_data_memory_wire;   
     end
     
-
     ////////////////////////////////////////
     //    Register File Instantiation     //
     ////////////////////////////////////////
@@ -263,9 +332,9 @@ module phoeniX
         .CLK(CLK),
         .reset(reset),
 
-        .read_enable_1(1'b0 & read_enable_1),
-        .read_enable_2(1'b0 & read_enable_2),
-        .write_enable(1'b0 & write_enable_writeback_reg),
+        .read_enable_1(read_enable_1_decode_wire),
+        .read_enable_2(read_enable_2_decode_wire),
+        .write_enable(write_enable_writeback_reg),
 
         .read_index_1(read_index_1_decode_wire),
         .read_index_2(read_index_2_decode_wire),
--- phoeniX_Testbench.v
+++ phoeniX_Testbench.v
@@ -18,19 +18,31 @@ module phoeniX_Testbench;
     always #1 CLK_MEM = ~CLK_MEM;
     always #6 CLK = ~CLK;
 
+    // Debug Wires for Register File
+
+    wire [31 : 0] x0 	= uut.register_file.Registers[0];
+    wire [31 : 0] x1 	= uut.register_file.Registers[1];
+    wire [31 : 0] x2 	= uut.register_file.Registers[2];
+    wire [31 : 0] x3 	= uut.register_file.Registers[3];
+    wire [31 : 0] x4 	= uut.register_file.Registers[4];
+    wire [31 : 0] x5 	= uut.register_file.Registers[5];
+    wire [31 : 0] x10 	= uut.register_file.Registers[10];
+    wire [31 : 0] x11 	= uut.register_file.Registers[11];
+    wire [31 : 0] x12 	= uut.register_file.Registers[12];
+        
     initial
     begin
         $dumpfile("phoeniX.vcd");
         $dumpvars(0, phoeniX_Testbench);
 
-        $readmemh("Instruction_Memory.txt", uut.fetch_unit.instruction_memory.Memory);
+        $readmemh("Test_RV32I_HEX.txt", uut.fetch_unit.instruction_memory.Memory);
 
         // Reset
         #24
         reset = 1'b1;
         #12
         reset = 1'b0;
-        #144
+        #1440
         $finish;
     end
 endmodule
