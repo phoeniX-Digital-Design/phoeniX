1bda6846d2fe4fad933059c368e40b983a6f4925
Faraz <farazghoreishy@gmail.com>
2023-08-21 11:19:46 +0330

    updating sum1to100 asm code

--- Sample Codes/Test_RV32I_sum1to100.s
+++ Sample Codes/Test_RV32I_sum1to100.s
@@ -1,10 +1,7 @@
-        add x10, x0, x0
-        add x14, x10, x0 
-        addi x12, x10, 10
-        add x13, x10, x0 
-LOOP :  add x14, x13, x14
-        addi x13, x13, 1
-        blt x13, x12, LOOP
-        add x10, x14, x0 
-        sw x0, 100(x10)
-        lw x17, 100(x0)
+        addi    x12,    x0,    101
+        add     x13,    x0,    x0 
+        add     x14,    x0,     x0 
+LOOP:   add     x14,    x13,    x14
+        addi    x13,    x13,    1
+        blt     x13,    x12,    LOOP
+        sw      x14,    100(x0)
