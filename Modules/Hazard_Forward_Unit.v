1f1844d6171e21109f165489bd7f2f7628d31941
Faraz <farazghoreishy@gmail.com>
2023-08-21 16:28:33 +0330

    Add logic for hazard detection and forwarding unit

--- Modules/Hazard_Forward_Unit.v
+++ Modules/Hazard_Forward_Unit.v
@@ -6,33 +6,27 @@
 
 module Hazard_Forward_Unit 
 (
-    input  [5:0] rs1,           // Register source 1
-    input  [5:0] rs2,           // Register source 2
-    input  [5:0] id_ex_rd,      // Destination register in the ID/EX pipeline stage
-    input  [5:0] if_id_rs1,     // Register source 1 in the IF/ID pipeline stage
-    input  [5:0] if_id_rs2,     // Register source 2 in the IF/ID pipeline stage
+    input [4 : 0] source_index,          
+    
+    input [4 : 0] destination_index_1,
+    input [4 : 0] destination_index_2,
+    input [4 : 0] destination_index_3,
 
-    input  mem_read,            // Memory Read signal from the Control Unit
-    input  mem_write,           // Memory Write signal from the Control Unit
-
-    output hazard,              // Hazard detection signal
-
-    output forward_exe_mux1,    // Forward data from execution satge - mux 1
-    output forward_exe_mux2,    // Forward data from execution satge - mux 2
-    output forward_mem_mux1,    // Forward data from memory satge - mux 1
-    output forward_mem_mux2     // Forward data from memory satge - mux 2
+    input [31 : 0] data_1,
+    input [31 : 0] data_2,
+    input [31 : 0] data_3,
+    
+    output reg [31 : 0] forward_data
 );
 
-    wire data_hazard;       // Data hazard signal
-    wire control_hazard;    // Data hazard signal
-
-    // Detect Data Hazard
-    assign data_hazard = ((rs1 == id_ex_rd) || (rs2 == id_ex_rd));
-
-    // Detect Control Hazard
-    assign control_hazard = mem_read || mem_write;
-
-    // Hazard Detection Signal
-    assign hazard = data_hazard || control_hazard;
-
+    always @(*) 
+    begin
+        if (source_index == destination_index_1)
+            forward_data <= data_1;
+        else if (source_index == destination_index_2)
+            forward_data <= data_2;
+        else if (source_index == destination_index_3)
+            forward_data <= data_3;
+    end
+    
 endmodule
