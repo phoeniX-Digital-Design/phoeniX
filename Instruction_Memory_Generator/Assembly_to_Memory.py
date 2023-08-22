53acd8ac3882eaa73926ddffb80387213ea1720c
Arvin Delavari <arvin7807@gmail.com>
2023-08-22 22:38:05 +0330

    Minor changes in python script

--- Instruction_Memory_Generator/Assembly_to_Memory.py
+++ Instruction_Memory_Generator/Assembly_to_Memory.py
@@ -1,6 +1,6 @@
 import os
 text = """
-RISC-V assembly code to phoeniX instruction memory
+RISC-V assembly code to phoeniX core input
 1) Write, simulate and modify your assembly code in Venus simulator.
 2) In 'VENUS OPTIONS' select 'Assembly' and save the output file as a text file (.txt).
 3) Enter the created text file name.
