7306985834c210244f0b3e2e4170938c347abfd7
Arvin Delavari <arvin7807@gmail.com>
2023-08-22 12:41:23 +0330

    Minor chnages in max of array assembly code

--- Sample_Codes/max_array.s
+++ Sample_Codes/max_array.s
@@ -1,20 +1,20 @@
 .text
 _boot:
-    jal x0,find_max
+    jal x0, find_max
 
     find_max:
         lw   x8, 1000(x0)           # maxElement = mem[1000]
         add  x9, x0, x0             # i
 
-        Loop:
+        loop:
             addi x9, x9, 4          # i += 4
             slti x6, x9, 40         # check if 10 elements are traversed (40 = 4 * 10)
             beq  x6, x0, end_loop   # if 10 elements are traversed, jump to EndLoop
             lw   x18, 1000(x9)      # element = mem[i]
             slt  x6, x8, x18        # check if element is greater than maxElement
-            beq  x6, x0, Loop       # if element is not greater than maxElement, jump to Loop
+            beq  x6, x0, loop       # if element is not greater than maxElement, jump to Loop
             add  x8, x18, x0        # maxElement = element
-            jal  x0,Loop            # jump to Loop
+            jal  x0, loop            # jump to Loop
 
         end_loop:
             sw x8, 2000(x0)         # mem[2000] = maxElement
