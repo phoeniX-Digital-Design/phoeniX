7c1a10ac65a3fd104f27f5d305533ac7762ac81d
Arvin Delavari <arvin7807@gmail.com>
2023-08-23 14:47:55 +0330

    Fix rv32im.sh

--- Sample_Codes/C codes/RV32 shell-script/rv32im.sh
+++ Sample_Codes/C codes/RV32 shell-script/rv32im.sh
@@ -1,7 +1,8 @@
 riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o code_name.o code_name.c 
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o load.o load.S
 riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o syscalls.o syscalls.c
 
-riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -Wl,--gc-sections -o firmware.elf code_name.o syscalls.o -T riscv.ld -lstdc++
+riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -Wl,--gc-sections -o firmware.elf load.o code_name.o syscalls.o -T riscv.ld -lstdc++
 chmod -x firmware.elf
 
 riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -nostdlib -o start.elf start.S -T start.ld -lstdc++
@@ -9,8 +10,11 @@ chmod -x start.elf
 
 riscv64-unknown-elf-objcopy -O verilog start.elf start.tmp
 riscv64-unknown-elf-objcopy -O verilog firmware.elf firmware.tmp
-
 cat start.tmp firmware.tmp > firmware.hex
 python3 hex8tohex32.py firmware.hex > firmware32.hex
 python3 phoeniX_firmware.py
 rm -f start.tmp firmware.tmp
+
+iverilog -o testbench.vvp testbench.v picorv32.v
+chmod -x testbench.vvp
+vvp -N testbench.vvp
