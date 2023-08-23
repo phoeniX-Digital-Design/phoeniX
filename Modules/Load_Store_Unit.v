a9cf0b55760acf5cc02a8ec6126dd44b7e7119ab
Faraz <farazghoreishy@gmail.com>
2023-08-23 20:21:09 +0330

    fix define statement issues

--- Modules/Load_Store_Unit.v
+++ Modules/Load_Store_Unit.v
@@ -1,8 +1,35 @@
 // `include "Memory_Interface.v"
 // `include "..\\Src\\Memory_Interface.v"
 `ifndef OPCODES
-    `include "Modules\\Opcodes.v"
-`endif
+    `define LOAD        7'b00_000_11
+    `define LOAD_FP     7'b00_001_11
+    `define custom_0    7'b00_010_11
+    `define MISC_MEM    7'b00_011_11
+    `define OP_IMM      7'b00_100_11
+    `define AUIPC       7'b00_101_11
+    `define OP_IMM_32   7'b00_110_11
+
+    `define STORE       7'b01_000_11
+    `define STORE_FP    7'b01_001_11
+    `define custom_1    7'b01_010_11
+    `define AMO         7'b01_011_11
+    `define OP          7'b01_100_11
+    `define LUI         7'b01_101_11
+    `define OP_32       7'b01_110_11
+
+    `define MADD        7'b10_000_11
+    `define MSUB        7'b10_001_11
+    `define NMSUB       7'b10_010_11
+    `define NMADD       7'b10_011_11
+    `define OP_FP       7'b10_100_11
+    `define custom_2    7'b10_110_11
+
+    `define BRANCH      7'b11_000_11
+    `define JALR        7'b11_001_11
+    `define JAL         7'b11_011_11
+    `define SYSTEM      7'b11_100_11
+    `define custom_3    7'b11_110_11
+`endif 
 
 `define BYTE                3'b000
 `define HALFWORD            3'b001
