1546d8b5a1ca84afecb2ce862142c901131f2c29
Arvin Delavari <arvin7807@gmail.com>
2023-08-23 10:49:57 +0330

    Add fibonacci C code file (GCC toolchain)

---
+++ Sample_Codes/C codes/fibonacci/rv32im.sh
@@ -0,0 +1,13 @@
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o fibonacci.o fibonacci.c 
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o syscalls.o syscalls.c
+
+riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -Wl,--gc-sections -o firmware.elf fibonacci.o syscalls.o -T riscv.ld -lstdc++
+chmod -x firmware.elf
+riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -nostdlib -o start.elf start.S -T start.ld -lstdc++
+chmod -x start.elf
+riscv64-unknown-elf-objcopy -O verilog start.elf start.tmp
+riscv64-unknown-elf-objcopy -O verilog firmware.elf firmware.tmp
+cat start.tmp firmware.tmp > firmware.hex
+python3 hex8tohex32.py firmware.hex > firmware32.hex
+rm -f start.tmp firmware.tmp
+
