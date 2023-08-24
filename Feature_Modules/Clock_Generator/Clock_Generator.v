cf621207ca25c904c36eb11bd551efdf0a4194e3
Arvin Delavari <arvin7807@gmail.com>
2023-08-24 20:52:20 +0330

    Add clock generator to feature modules

---
+++ Feature_Modules/Clock_Generator/Clock_Generator.v
@@ -0,0 +1,23 @@
+module Clock_Generator #(parameter DIVIDER = 10)
+(
+    input        reset,
+    input        clk_in,    // Input clock
+    output reg   clk_out    // Output clock
+);
+
+    reg [DIVIDER - 1 : 0] count = 0;        // Counter for clock division
+
+    always @(posedge reset) begin           // Always needs a restart signal tp initialize output clock
+        clk_out <= 1'b0;
+    end
+
+    always @(posedge clk_in) begin
+        if (count == DIVIDER - 1) begin     // Reset counter when it reaches the division factor
+            count <= 0;
+            clk_out <= ~clk_out; 
+        end else
+            count <= count + 1;
+    end
+
+endmodule
+
---
+++ Feature_Modules/Clock_Generator/Clock_Generator_tb.v
@@ -0,0 +1,30 @@
+`include "Clock_Generator.v"
+
+module Clock_Generator_TB;
+
+    reg  reset;
+    reg  clk_in;
+    wire clk_out;
+
+    Clock_Generator #(.DIVIDER(10)) uut 
+    (
+        .reset(reset),
+        .clk_in(clk_in),
+        .clk_out(clk_out)
+    );
+
+    always begin
+        #5 clk_in = ~clk_in;  // Toggle the input clock every 5 time units
+    end
+
+    initial begin
+        $dumpfile("Clock_Generator.vcd");
+        $dumpvars(0, Clock_Generator_TB);
+        reset  = 1;   // Restart : Initialize output clock
+        clk_in = 0;   // Initialize input clock
+        #20 
+        reset = 0;    // Start cLock generation
+        #2000 $finish;
+    end
+
+endmodule
