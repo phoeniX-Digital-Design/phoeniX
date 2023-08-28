be2198618550edf9ad19a244ce816126a1591ef8
Arvin Delavari <arvin7807@gmail.com>
2023-08-28 16:02:28 +0330

    Create GCC to assembly (Venus format) script

---
+++ Sample_Codes/Fibonacci.s
@@ -0,0 +1,34 @@
+	.text
+	.globl	main
+main:
+	addi	sp,sp,-48
+	sw	s0,44(sp)
+	addi	s0,sp,48
+	sw	zero,-20(s0)
+	li	a5,1
+	sw	a5,-24(s0)
+	li	a5,10
+	sw	a5,-32(s0)
+	sw	zero,-28(s0)
+	j	.L2
+.L3:
+	lw	a4,-20(s0)
+	lw	a5,-24(s0)
+	add	a5,a4,a5
+	sw	a5,-36(s0)
+	lw	a5,-24(s0)
+	sw	a5,-20(s0)
+	lw	a5,-36(s0)
+	sw	a5,-24(s0)
+	lw	a5,-28(s0)
+	addi	a5,a5,1
+	sw	a5,-28(s0)
+.L2:
+	lw	a4,-28(s0)
+	lw	a5,-32(s0)
+	blt	a4,a5,.L3
+	li	a5,0
+	mv	a0,a5
+	lw	s0,44(sp)
+	addi	sp,sp,48
+	jr	ra
