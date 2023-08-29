5703f9d1a0f6ee6fec01ed70dd373d0092f23979
Faraz <farazghoreishy@gmail.com>
2023-08-29 13:00:06 +0330

    fix type issue for JALR

--- Modules/Jump_Branch_Unit.v
+++ Modules/Jump_Branch_Unit.v
@@ -1,3 +1,34 @@
+`ifndef OPCODES
+    `define LOAD        7'b00_000_11
+    `define LOAD_FP     7'b00_001_11
+    `define custom_0    7'b00_010_11
+    `define MISC_MEM    7'b00_011_11
+    `define OP_IMM      7'b00_100_11
+    `define AUIPC       7'b00_101_11
+    `define OP_IMM_32   7'b00_110_11
+
+    `define STORE       7'b01_000_11
+    `define STORE_FP    7'b01_001_11
+    `define custom_1    7'b01_010_11
+    `define AMO         7'b01_011_11
+    `define OP          7'b01_100_11
+    `define LUI         7'b01_101_11
+    `define OP_32       7'b01_110_11
+
+    `define MADD        7'b10_000_11
+    `define MSUB        7'b10_001_11
+    `define NMSUB       7'b10_010_11
+    `define NMADD       7'b10_011_11
+    `define OP_FP       7'b10_100_11
+    `define custom_2    7'b10_110_11
+
+    `define BRANCH      7'b11_000_11
+    `define JALR        7'b11_001_11
+    `define JAL         7'b11_011_11
+    `define SYSTEM      7'b11_100_11
+    `define custom_3    7'b11_110_11
+`endif
+
 `ifndef INSTRUCTION_TYPES
     `define R_TYPE 0
     `define I_TYPE 1
@@ -16,6 +47,7 @@
 
 module Jump_Branch_Unit 
 (
+    input [6 : 0] opcode,
     input [2 : 0] funct3,
     input [2 : 0] instruction_type,
 
@@ -49,7 +81,7 @@ module Jump_Branch_Unit
             else
                 branch_enable = 1'b0;
 
-            if (instruction_type == `J_TYPE)
+            if (opcode == `JAL || opcode == `JALR)
                 jump_enable = 1'b1;
             else
                 jump_enable = 1'b0;
