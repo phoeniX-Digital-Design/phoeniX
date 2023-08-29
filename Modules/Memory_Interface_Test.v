8f034d4f6cc81086695930f52f0530c11657128e
Arvin Delavari <arvin7807@gmail.com>
2023-08-29 09:47:54 +0330

    Add a 1KB cache memory in new interface module

--- Modules/Memory_Interface_Test.v
+++ Modules/Memory_Interface_Test.v
@@ -1,19 +1,19 @@
 module Memory_Interface_Test 
 (
-    input wire [31 : 0] address,
-    input wire [31 : 0] write_data,
-    input wire write_enable,
-    input wire read_enable,
+    input [31 : 0] address,
+    input [31 : 0] write_data,
+    input write_enable,
+    input read_enable,
     output reg [31 : 0] read_data
 );
 
-  reg [31 : 0] memory [0 : 31];
+  reg [31 : 0] cache_memory [0 : 255]; // 1KB chache memory inside the interface module
 
   always @(posedge read_enable or negedge write_enable) begin
     if (read_enable)
-      read_data <= memory[address];
+      read_data <= cache_memory[address];
     else if (!write_enable)
-      memory[address] <= write_data;
+      cache_memory[address] <= write_data;
   end
 
 endmodule
