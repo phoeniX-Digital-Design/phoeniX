4194a05800f21ca9a0de7cee942311e00fc47e52
Arvin Delavari <arvin7807@gmail.com>
2023-08-30 10:30:48 +0330

    Minor changes in AXI4_read module

--- Modules_Feature/AXI4_Lite_Interface/AXI4_read.v
+++ Modules_Feature/AXI4_Lite_Interface/AXI4_read.v
@@ -3,39 +3,39 @@ module AXI4_read #(parameter ADDRESS_WIDTH = 2)
     input axi_clk,
     input resetn,    // AXI is reset low
 
-    // Read address channel
+    // read address channel
     input [ADDRESS_WIDTH - 1 : 0] read_addr,
     input read_addr_valid,
     output reg read_addr_ready,
 
-    // Read data channel
+    // read data channel
     output reg [31 : 0] read_data,
     output reg read_data_valid,
     input read_data_ready,
 
-    // Read response channel
+    // read response channel
     output [1 : 0] read_resp,
     input read_resp_ready,
     output reg read_resp_valid,
 
-    input [31 : 0] data_in,                     // Data input from external logic
-    input [ADDRESS_WIDTH - 1 : 0] addr_in,      // Address input from external logic
-    input data_valid                            // Signal indicating input data and address are valid
+    input [31 : 0] data_in,                     // data input from external logic
+    input [ADDRESS_WIDTH - 1 : 0] addr_in,      // address input from external logic
+    input data_valid                            // signal indicating input data and address are valid
 );
 
     reg addr_done;
     reg data_done;
 
-    // Flip flops for latching data
+    // flip flops for latching data
     reg [31 : 0] data_latch;
     reg [ADDRESS_WIDTH - 1 : 0] addr_latch;
 
     always @(*) begin
         assign read_data = data_latch;
     end
-    assign read_resp = {2'b0}; // Always indicate OKAY status for reads
+    assign read_resp = {2'b0}; // always indicate OKAY status for reads
 
-    // Read address handshake
+    // read address handshake
     always @(posedge axi_clk)
     begin
         if (~resetn | (read_addr_valid & read_addr_ready))
@@ -44,7 +44,7 @@ module AXI4_read #(parameter ADDRESS_WIDTH = 2)
             read_addr_ready <= 1;
     end
 
-    // Read data handshake
+    // read data handshake
     always @(posedge axi_clk)
     begin
         if (~resetn | (read_data_valid & read_data_ready))
@@ -53,43 +53,43 @@ module AXI4_read #(parameter ADDRESS_WIDTH = 2)
             read_data_valid <= 1;
     end
 
-    // Keep track of which handshakes completed
+    // keep track of which handshakes completed
     always @(posedge axi_clk)
     begin
-        if (resetn == 0 || (addr_done & data_done)) // Reset or both phases done
+        if (resetn == 0 || (addr_done & data_done)) // reset or both phases done
         begin
             addr_done <= 0;
             data_done <= 0;
         end
         else
         begin    
-            if (read_addr_valid & read_addr_ready) // Look for address handshake
+            if (read_addr_valid & read_addr_ready) // look for address handshake
                 addr_done <= 1;
             
-            if (read_data_valid & read_data_ready) // Look for data handshake
+            if (read_data_valid & read_data_ready) // look for data handshake
                 data_done <= 1;    
         end
     end
 
-    // Latching logic
+    // latching logic
     always @(posedge axi_clk)
     begin
-        if (resetn == 0) // Reset values
+        if (resetn == 0) // reset values
         begin
             data_latch <= 32'd0;
             addr_latch <= {ADDRESS_WIDTH{1'b0}};
         end
         else
         begin
-            if (read_data_valid & read_data_ready) // Look for data handshake
+            if (read_data_valid & read_data_ready) // look for data handshake
                 data_latch <= data_in;
             
-            if (read_addr_valid & read_addr_ready) // Look for address handshake
+            if (read_addr_valid & read_addr_ready) // look for address handshake
                 addr_latch <= read_addr;
         end
     end
 
-    // Read response logic
+    // read response logic
     always @(posedge axi_clk)
     begin    
         if (resetn == 0 | (read_resp_valid & read_resp_ready))
