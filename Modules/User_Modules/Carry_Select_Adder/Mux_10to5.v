2b93dd6863adb031f312508bbdb4caef6ba481c2
ArvinDelavari <arvin7807@gmail.com>
2023-10-04 12:55:54 +0330

    ✅ Verify CSA 10:5 multiplexer

--- Modules/User_Modules/Carry_Select_Adder/Mux_10to5.v
+++ Modules/User_Modules/Carry_Select_Adder/Mux_10to5.v
@@ -1,18 +1,22 @@
-module Mux_10to5 
+module MUX_10to5 
 (
-    input [4 : 0] data_in_1,        // RCA output
-    input [4 : 0] data_in_2,        // Basic Block output
-    input select,                   // Input carry
+    input [3 : 0] data_in_1,        // RCA output
+    input [3 : 0] data_in_2,        // Basic Block output
+    input select,                   // Select pin = Carry
+
+    input carry_in_1,               // Input carry 1
+    input carry_in_2,               // Input carry 2
+
     output reg [3 : 0] data_out,    // Data out
-    output reg carry                // Output carry
+    output reg carry_out            // Output carry
 );
 
     always @(*) 
     begin
         case (select)
-            1'b0: begin data_out = data_in_1 [3 : 0]; carry = data_in_1 [4]; end
-            1'b1: begin data_out = data_in_2 [3 : 0]; carry = data_in_2 [4];end
-            default: data_out = 5'bz;
+            1'b0: begin data_out = data_in_1; carry_out = carry_in_1; end
+            1'b1: begin data_out = data_in_2; carry_out = carry_in_2; end
+            default: begin data_out = 4'bz; carry_out = 1'bz; end
         endcase
     end
 
