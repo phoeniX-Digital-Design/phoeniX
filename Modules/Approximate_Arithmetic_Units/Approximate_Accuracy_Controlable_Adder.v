4b9bfbdb5533a526962cb94747949c6823b0c69f
Faraz <farazghoreishy@gmail.com>
2023-10-05 16:18:57 +0330

    Add AC_APX_Adder

---
+++ Modules/Approximate_Arithmetic_Units/Approximate_Accuracy_Controlable_Adder.v
@@ -0,0 +1,287 @@
+module ACAPX_Adder 
+#(
+    parameter LEN = 32,
+    parameter APX_LEN = 8         // Valid Options for APX_LEN : 4, 8, 12, 16, ...
+)
+(
+    input [APX_LEN - 1 : 0] Er,
+    input [LEN - 1 : 0] A,
+    input [LEN - 1 : 0] B,
+    input Cin,
+
+    output [LEN - 1 : 0] Sum,
+    output Cout
+);
+
+    wire [LEN - 1 : 0] C;
+    
+    ////////////////////
+    //    [3 : 0]     //
+    ////////////////////
+
+    Error_Configurable_Ripple_Carry_Adder #(.LEN(4)) EC_RCA_1 
+    (
+        .Er(Er[3  : 0]),
+        .A(A[3 : 0]), 
+        .B(B[3 : 0]), 
+        .Cin(Cin), 
+        .Sum(Sum[3 : 0]), 
+        .Cout(C[3])
+    );
+    
+    ////////////////////
+    //    [31 : 4]    //
+    ////////////////////
+
+    genvar i;
+    generate
+        
+        // ------------------- //
+        // Approximate Circuit //
+        // ------------------- //
+
+        for (i = 4; i < APX_LEN; i = i + 4)
+        begin
+            wire HA_Carry;
+            wire EC_RCA_Carry;
+            wire [i + 3 : i] EC_RCA_Output;
+
+            Half_Adder HA
+            (
+                .A(A[i]), 
+                .B(B[i]),
+                .Sum(EC_RCA_Output[i]),
+                .Cout(HA_Carry)
+            );
+
+            Error_Configurable_Ripple_Carry_Adder
+            #(
+                .LEN(3)
+            )
+            EC_RCA
+            (
+                .Er(Er[i + 3 : i + 1]),
+                .A(A[i + 3 : i + 1]), 
+                .B(B[i + 3 : i + 1]), 
+                .Cin(HA_Carry),
+                .Sum(EC_RCA_Output[i + 3 : i + 1]),
+                .Cout(EC_RCA_Carry)
+            );
+
+            wire BU_Carry;
+            wire [i + 3 : i] BU_Output;
+
+            Basic_Unit BU_1 (.A(EC_RCA_Output), .B(BU_Output), .C0(BU_Carry));
+
+            Mux_2to1 
+            #(
+                .LEN(5)
+            )
+            MUX
+            (
+                .data_in_1({EC_RCA_Carry, EC_RCA_Output}),
+                .data_in_2({BU_Carry || EC_RCA_Carry, BU_Output}),
+                .select(C[i - 1]),
+                .data_out({C[i + 3], Sum[i + 3 : i]})
+            );
+        end
+        
+        // ------------- //
+        // Exact Circuit //
+        // ------------- //
+
+        for (i = APX_LEN; i < LEN; i = i + 4)
+        begin
+            wire HA_Carry;
+            wire RCA_Carry;
+            wire [i + 3 : i] RCA_Output;
+
+            Half_Adder HA
+            (
+                .A(A[i]), 
+                .B(B[i]),
+                .Sum(RCA_Output[i]),
+                .Cout(HA_Carry)
+            );
+
+            Ripple_Carry_Adder
+            #(
+                .LEN(3)
+            )
+            RCA
+            (
+                .A(A[i + 3 : i + 1]), 
+                .B(B[i + 3 : i + 1]), 
+                .Cin(HA_Carry),
+                .Sum(RCA_Output[i + 3 : i + 1]),
+                .Cout(RCA_Carry)
+            );
+
+            wire BU_Carry;
+            wire [i + 3 : i] BU_Output;
+
+            Basic_Unit BU_1 (.A(RCA_Output), .B(BU_Output), .C0(BU_Carry));
+
+            Mux_2to1 
+            #(
+                .LEN(5)
+            )
+            MUX
+            (
+                .data_in_1({RCA_Carry, RCA_Output}),
+                .data_in_2({BU_Carry || RCA_Carry, BU_Output}),
+                .select(C[i - 1]),
+                .data_out({C[i + 3], Sum[i + 3 : i]})
+            );
+        end
+
+    endgenerate
+    
+    assign Cout = C[LEN - 1];
+endmodule
+
+module Basic_Unit 
+(
+    input  [3 : 0] A,
+    output [4 : 1] B,
+    output C0
+);
+
+    assign B[1] =  ~A[0];
+    assign B[2] = A[1] ^ A[0];
+    wire C1 = A[1] & A[0];
+    wire C2 = A[2] & A[3];
+    wire C0 = C1 & C2;
+    wire C3 = C1 & A[2];
+    assign B[3] = A[2] ^ C1;
+    assign B[4] = A[3] ^ C3;
+
+endmodule
+
+module Mux_2to1
+#(
+    parameter LEN = 5
+) 
+(
+    input [LEN - 1 : 0] data_in_1,        
+    input [LEN - 1 : 0] data_in_2,        
+    input select,                   
+
+    output reg [LEN - 1: 0] data_out            
+);
+
+    always @(*) 
+    begin
+        case (select)
+            1'b0: begin data_out = data_in_1; end
+            1'b1: begin data_out = data_in_2; end
+            default: begin data_out = {LEN{1'bz}}; end
+        endcase
+    end
+
+endmodule
+
+module Error_Configurable_Ripple_Carry_Adder 
+#(
+    parameter LEN = 4
+) 
+(
+    input [LEN - 1 : 0] Er,
+    input [LEN - 1 : 0] A,
+    input [LEN - 1 : 0] B,
+    input Cin,
+
+    output [LEN - 1 : 0] Sum,
+    output Cout
+);
+    wire [LEN : 0] Carry;
+    assign Carry[0] = Cin;
+
+    genvar i;
+    generate
+        for (i = 0; i < LEN; i = i + 1)
+        begin
+            Error_Configurable_Full_Adder ECFA 
+            (
+                .Er(Er[i]),
+                .A(A[i]), 
+                .B(B[i]), 
+                .Cin(Carry[i]), 
+                .Sum(Sum[i]), 
+                .Cout(Carry[i + 1])
+            );
+        end
+    assign Cout = Carry[LEN];
+    endgenerate
+    
+endmodule
+
+module Ripple_Carry_Adder 
+#(
+    parameter LEN = 4
+) 
+(
+    input [LEN - 1 : 0] A,
+    input [LEN - 1 : 0] B,
+    input Cin,
+
+    output [LEN - 1 : 0] Sum,
+    output Cout
+);
+    wire [LEN : 0] Carry;
+    assign Carry[0] = Cin;
+
+    genvar i;
+    generate
+        for (i = 0; i < LEN; i = i + 1)
+        begin
+            Full_Adder FA 
+            (
+                .A(A[i]), 
+                .B(B[i]), 
+                .Cin(Carry[i]), 
+                .Sum(Sum[i]), 
+                .Cout(Carry[i + 1])
+            );
+        end
+    assign Cout = Carry[LEN];
+    endgenerate
+    
+endmodule
+
+module Error_Configurable_Full_Adder
+(
+    input Er,
+    input A,
+    input B, 
+    input Cin,
+
+    output Sum, 
+    output Cout
+);
+    assign Sum = ~(Er && (A ^ B) && Cin) && ((A ^ B) || Cin);
+    assign Cout = (Er && B && Cin) || ((B || Cin) && A);
+endmodule
+
+module Full_Adder 
+(
+    input A,
+    input B,
+    input Cin,
+
+    output Sum,
+    output Cout
+);
+    assign Sum = A ^ B ^ Cin;
+    assign Cout = (A && B) || (A && Cin) || (B && Cin); 
+endmodule
+
+module Half_Adder (
+    input A,
+    input B, 
+    output Sum, 
+    output Cout
+);
+    assign Sum = A ^ B;
+    assign Cout = A & B;
+endmodule
