d744ad91ba5aad6fac0f13173d30e488235f1d0e
ArvinDelavari <arvin7807@gmail.com>
2023-10-05 13:43:37 +0330

    ✨ Added REM and REMU support to sample divider

--- Modules/User_Modules/Sample_Divider/Sample_Divider.v
+++ Modules/User_Modules/Sample_Divider/Sample_Divider.v
@@ -5,7 +5,8 @@ module Sample_Divider
     input [31 : 0] input_2, 
     input [7  : 0] accuracy, 
     output busy, 
-    output reg [31 : 0] result
+    output reg [31 : 0] result,
+    output reg [31 : 0] remainder
 );
     reg [31 : 0] output_div;
 
@@ -14,18 +15,22 @@ module Sample_Divider
         if (accuracy == 0)
         begin 
             output_div = input_1 / input_2;
+            remainder  = input_1 % input_2;
         end
         else if (accuracy == 1)
         begin 
             output_div = (input_1 / input_2) - 1;
+            remainder  = input_1 % input_2;
         end
         else if (accuracy == 2)
         begin 
             output_div = (input_1 / input_2) - 2;
+            remainder  = input_1 % input_2;
         end
         // No accuracy control logic in divider
         else begin 
             output_div = input_1 / input_2;
+            remainder  = input_1 % input_2;
         end
     end
     
