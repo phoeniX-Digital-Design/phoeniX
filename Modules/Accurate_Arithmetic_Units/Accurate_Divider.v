8a9956a2be5e26773c5d7f993996cbdf8dbf9b15
Faraz <farazghoreishy@gmail.com>
2023-10-18 13:36:11 +0330

    Minor Changes

--- Modules/Accurate_Arithmetic_Units/Accurate_Divider.v
+++ Modules/Accurate_Arithmetic_Units/Accurate_Divider.v
@@ -30,7 +30,8 @@
             assign latched_rem_result = rem_result;
         end
     end
-    always @(*) begin
+    always @(*) 
+    begin
         if ((output_ready == 1) && (busy == 0))
         begin
             div = latched_div_result;
@@ -45,32 +46,32 @@
     assign busy = active;
 
     // The state machine  
-    always @(posedge CLK) begin  
-
-            if (active) 
+    always @(posedge CLK)
+    begin 
+        if (active) 
+        begin  
+            // remune an iteration of the divide.  
+            if (sub[32] == 0) 
             begin  
-                // remun an iteration of the divide.  
-                if (sub[32] == 0) 
-                begin  
-                    work  <= sub[31 : 0];
-                    result <= {result[30 : 0], 1'b1};
-                end  
-                else 
-                begin  
-                    work <= {work[30 : 0], result[31]};
-                    result <= {result[30 : 0], 1'b0};
-                end  
-                if (cycle == 0) begin active <= 0; end  
-                cycle <= cycle - 5'd1;  
-            end
-            else begin  
-                // Set up for an unsigned divide.  
-                cycle  <= 5'd31;  
-                result <= operand_1;  
-                denom  <= operand_2;  
-                work   <= 32'b0;  
-                active <= 1;  
+                work  <= sub[31 : 0];
+                result <= {result[30 : 0], 1'b1};
             end  
+            else 
+            begin  
+                work <= {work[30 : 0], result[31]};
+                result <= {result[30 : 0], 1'b0};
+            end  
+            if (cycle == 0) begin active <= 0; end  
+            cycle <= cycle - 5'd1;  
+        end
+        else begin  
+            // Set up for an unsigned divide.  
+            cycle  <= 5'd31;  
+            result <= operand_1;  
+            denom  <= operand_2;  
+            work   <= 32'b0;  
+            active <= 1;  
         end  
+    end  
 
 endmodule   
