69c615107efd068c5dc88258653ca4d7102bc81d
Faraz <farazghoreishy@gmail.com>
2023-11-01 13:09:19 +0330

    🚧 fix instruction

--- Software/Sample_Assembly_Codes/sum1to100_approximate/sum1to100_approximate.s
+++ Software/Sample_Assembly_Codes/sum1to100_approximate/sum1to100_approximate.s
@@ -2,7 +2,7 @@
         addi    x12,    x0,    6
         add     x13,    x0,    x0 
         add     x14,    x0,     x0 
-LOOP:   mul     x14,    x13,    x14
+LOOP:   add     x14,    x13,    x14
         addi    x13,    x13,    1
         blt     x13,    x12,    LOOP
         sw      x14,    100(x0)
