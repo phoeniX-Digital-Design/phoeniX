6ffdfe4f808cedb81847e32ddfc4fa68de87e541
Faraz <farazghoreishy@gmail.com>
2023-11-09 12:45:07 +0330

    🎨 Minor changes

--- Modules/Fetch_Unit.v
+++ Modules/Fetch_Unit.v
@@ -4,8 +4,11 @@ module Fetch_Unit
 
     input [31 : 0] PC,
 
-	input [31 : 0] address,         // Branch or Jump address generated in Address Generator
-	input jump_branch_enable,       // Generated in Branch Unit module
+	input [31 : 0] jump_branch_address,         
+	input jump_branch_enable,       
+    
+    // input [31 : 0] stall_address,
+    // input stall_enable,
 
     output reg [31 : 0] next_PC,    // next instruction PC output
     
@@ -16,7 +19,7 @@ module Fetch_Unit
     output  reg  memory_interface_enable,
     output  reg  memory_interface_state,
     output  reg  [31 : 0]   memory_interface_address,
-    output  reg  [3 : 0]    memory_interface_frame_mask
+    output  reg  [ 3 : 0]   memory_interface_frame_mask
 );
     localparam  READ    = 1'b0;
     localparam  WRITE   = 1'b1;
@@ -31,7 +34,8 @@ module Fetch_Unit
 
     always @(*)
     begin
-        if (jump_branch_enable) next_PC <= address;
+        if (jump_branch_enable) next_PC <= jump_branch_address;
+        // else if (stall_enable)  next_PC <= stall_address;
         else                    next_PC <= PC + 32'd4;
     end
 endmodule
