8d2bb182fdb08eb6e62e8a8b435f179e3cdf7238
Faraz <farazghoreishy@gmail.com>
2023-11-12 19:27:57 +0330

    ✅ Testing 30-bit incrementer

---
+++ Features/Incrementer/Incrementer_Testbench.v
@@ -0,0 +1,40 @@
+`include "Incrementer.v"
+
+module Incrementer_Testbench;
+    parameter WIDTH = 30;
+
+    reg  [WIDTH - 1 : 0] A;
+    wire [WIDTH - 1 : 0] B;
+
+    Incrementer uut 
+    (
+        .operand_1(A),
+        .result(B)
+    );
+    
+    initial begin
+
+        $dumpfile("Incrementer.vcd");
+        $dumpvars(0, Incrementer_Testbench);
+
+        A = $random;
+        #10
+        $display("A: %b", A);
+        $display("B: %b", B);
+        #10;
+
+        A = $random;
+        #10
+        $display("A: %b", A);
+        $display("B: %b", B);
+        #10;
+
+        A = $random;
+        #10
+        $display("A: %b", A);
+        $display("B: %b", B);
+        #10;
+
+        $finish;
+    end      
+endmodule
