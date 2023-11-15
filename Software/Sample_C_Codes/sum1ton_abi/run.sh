732482a397c0f77be74f6c7415a0f553339547a4
Faraz Ghoreishy <farazghoreishy@gmail.com>
2023-11-15 14:40:28 +0330

    🚚 Temporary move firmware files

---
+++ Software/Sample_C_Codes/sum1ton_abi/run.sh
@@ -0,0 +1,14 @@
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o sum1ton_abi.o sum1ton_abi.c 
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o sum1ton.o sum1ton.s
+
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o Firmware/syscalls.o Firmware/syscalls.c
+riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -Wl,--gc-sections -o firmware.elf sum1ton.o sum1ton_abi.o Firmware/syscalls.o -T Firmware/riscv.ld -lstdc++
+chmod -x firmware.elf
+riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -nostdlib -o start.elf Firmware/start.s -T Firmware/start.ld -lstdc++
+chmod -x start.elf
+riscv64-unknown-elf-objcopy -O verilog start.elf start.tmp
+riscv64-unknown-elf-objcopy -O verilog firmware.elf firmware.tmp
+cat start.tmp firmware.tmp > firmware.hex
+
+python3 Firmware/hex_converter.py firmware.mem > sum1ton_abi_firmware.hex
+
--- Software/Sample_C_Codes/sum1ton_abi/sum1ton.s
+++ Software/Sample_C_Codes/sum1ton_abi/sum1ton.s
@@ -3,13 +3,13 @@
 .type sum1ton, @function
 
 sum1ton:
-        add 	a4, a0, zero        // Initialize sum register a4 with 0x0
-        add 	a2, a0, a1          // store count of 10 in register a2. Register a1 is sum1toned with 0xa (decimal 10) from main program
-        add	    a3, a0, zero        // initialize intermediate sum register a3 by 0
+        add 	a4, a0, zero        
+        add 	a2, a0, a1          
+        add	    a3, a0, zero        
 
 loop:	
-        add 	a4, a3, a4          // Incremental addition
-        addi 	a3, a3, 1           // Increment intermediate register by 1	
-        blt 	a3, a2, loop        // If a3 is less than a2, branch to label named <loop>
-        add	    a0, a4, zero        // Store final result to register a0 so that it can be read by main program
+        add 	a4, a3, a4          
+        addi 	a3, a3, 1           
+        blt 	a3, a2, loop        
+        add	    a0, a4, zero        
         ret
