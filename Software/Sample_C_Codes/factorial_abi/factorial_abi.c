457384cbb6182c6d43f41a48ba607f7ffd23c3eb
Faraz Ghoreishy <farazghoreishy@gmail.com>
2023-11-22 17:43:21 +0330

    ✅ Testing ABI functions with makefile

--- Software/Sample_C_Codes/factorial_abi/factorial_abi.c
+++ Software/Sample_C_Codes/factorial_abi/factorial_abi.c
@@ -5,7 +5,7 @@ extern int factorial(int x, int y);
 int main()
 {
     int result = 1;
-    int count = 10;
-    result = factorial(0x1, count + 1);
+    int count = 5;
+    result = factorial(0x1, count);
     return 0;
 }
