15da89c82e927f4617f22dcd5f3f9d98ea83e83a
Faraz <farazghoreishy@gmail.com>
2023-12-30 19:33:02 +0330

    ⚡️ change edge sensivity of write action on registers

--- Modules/Register_File.v
+++ Modules/Register_File.v
@@ -29,7 +29,7 @@ module Register_File
             Registers[i] = {WIDTH{1'b0}};
     end
 	
-    always @(negedge clk)
+    always @(posedge clk)
     begin
         if (write_enable == 1'b1 && write_index != 0)
         begin
