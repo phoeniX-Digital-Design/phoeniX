524bfdb88f717424e069f6bd080e7f1d769a03b6
Faraz <farazghoreishy@gmail.com>
2023-12-30 19:25:21 +0330

    🎨 change output to reg type

--- Modules/Jump_Branch_Unit.v
+++ Modules/Jump_Branch_Unit.v
@@ -2,14 +2,14 @@
 
 module Jump_Branch_Unit 
 (
-    input [6 : 0] opcode,
-    input [2 : 0] funct3,
-    input [2 : 0] instruction_type,
+    input wire [ 6 : 0] opcode,
+    input wire [ 2 : 0] funct3,
+    input wire [ 2 : 0] instruction_type,
 
-    input [31 : 0] rs1,
-    input [31 : 0] rs2,
+    input wire [31 : 0] rs1,
+    input wire [31 : 0] rs2,
       
-    output jump_branch_enable     
+    output reg jump_branch_enable     
 );
 
     reg branch_enable;
@@ -18,23 +18,20 @@ module Jump_Branch_Unit
     always @(*) 
     begin
             if (instruction_type == `B_TYPE)  
-                casex ({funct3})
-                    `BEQ  : begin if ($signed(rs1) == $signed(rs2))   branch_enable = 1'b1; else  branch_enable = 1'b0; end 
-                    `BNE  : begin if ($signed(rs1) != $signed(rs2))   branch_enable = 1'b1; else  branch_enable = 1'b0; end                    
-                    `BLT  : begin if ($signed(rs1) <  $signed(rs2))   branch_enable = 1'b1; else  branch_enable = 1'b0; end                    
-                    `BGE  : begin if ($signed(rs1) >= $signed(rs2))   branch_enable = 1'b1; else  branch_enable = 1'b0; end                    
-                    `BLTU : begin if (rs1 < rs2)                      branch_enable = 1'b1; else  branch_enable = 1'b0; end
-                    `BGEU : begin if (rs1 >= rs2)                     branch_enable = 1'b1; else  branch_enable = 1'b0; end                    
-                    default: branch_enable = 1'b0;
+                case (funct3)
+                    `BEQ  : begin if ($signed(rs1) == $signed(rs2))   branch_enable = `ENABLE; else  branch_enable = `DISABLE; end 
+                    `BNE  : begin if ($signed(rs1) != $signed(rs2))   branch_enable = `ENABLE; else  branch_enable = `DISABLE; end                    
+                    `BLT  : begin if ($signed(rs1) <  $signed(rs2))   branch_enable = `ENABLE; else  branch_enable = `DISABLE; end                    
+                    `BGE  : begin if ($signed(rs1) >= $signed(rs2))   branch_enable = `ENABLE; else  branch_enable = `DISABLE; end                    
+                    `BLTU : begin if (rs1 < rs2)                      branch_enable = `ENABLE; else  branch_enable = `DISABLE; end
+                    `BGEU : begin if (rs1 >= rs2)                     branch_enable = `ENABLE; else  branch_enable = `DISABLE; end                    
+                    default: branch_enable = `DISABLE;
                 endcase
-            else
-                branch_enable = 1'b0;
+            else branch_enable = `DISABLE;
 
-            if (opcode == `JAL || opcode == `JALR)
-                jump_enable = 1'b1;
-            else
-                jump_enable = 1'b0;
-      end  
-
-      assign jump_branch_enable = jump_enable || branch_enable;
+            if (opcode == `JAL || opcode == `JALR) jump_enable = `ENABLE;
+            else jump_enable = `DISABLE;
+            
+            jump_branch_enable = jump_enable || branch_enable;
+    end 
 endmodule
