8bd77659d4e23bd497c2afbea8479dcc2f8e598a
ArvinDelavari <arvin7807@gmail.com>
2023-12-30 22:10:55 +0330

    ✨ add printf for debug in fibonacci sample code

--- Software/Sample_C_Codes/fibonacci/fibonacci.c
+++ Software/Sample_C_Codes/fibonacci/fibonacci.c
@@ -5,14 +5,15 @@ int main()
     int n1 = 0;
     int n2 = 1;
     int n3;
-    int count = 10;
+    int count = 7;
 
     for(int i = 2; i < count; i++)     
     {    
         n3 = n1 + n2;    
         n1 = n2;    
         n2 = n3;    
+        printf("%d \t", n3);
     }  
+    printf("\nNumber %d result of Fibonacci sequence is %d\n", count, n3);
     return 0;  
 }
-
