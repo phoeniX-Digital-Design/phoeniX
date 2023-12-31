3033cf6af0d81cced2f665e0828a868bffc059fb
ArvinDelavari <arvin7807@gmail.com>
2023-12-31 20:30:07 +0330

    🎨 Update factorial_abi code with printf debigging

--- Software/Sample_C_Codes/factorial_abi/factorial_abi.c
+++ Software/Sample_C_Codes/factorial_abi/factorial_abi.c
@@ -5,7 +5,8 @@ extern int factorial(int x, int y);
 int main()
 {
     int result = 1;
-    int count = 5;
+    int count = 3;
     result = factorial(0x1, count);
+    printf("Result of %d! is %d", count, result);
     return 0;
 }
