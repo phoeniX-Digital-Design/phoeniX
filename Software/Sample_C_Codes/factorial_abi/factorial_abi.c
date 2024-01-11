2bd54b742f563f221b19271b1733550735b9e0a5
Faraz <farazghoreishy@gmail.com>
2024-01-11 13:34:34 +0330

    ✅ new firmware with n=10

--- Software/Sample_C_Codes/factorial_abi/factorial_abi.c
+++ Software/Sample_C_Codes/factorial_abi/factorial_abi.c
@@ -5,8 +5,8 @@ extern int factorial(int x, int y);
 int main()
 {
     int result = 1;
-    int count = 3;
-    result = factorial(0x1, count);
-    // printf("Result of %d! is %d", count, result);
+    int n = 10;
+    result = factorial(0x1, n - 1);
+    // printf("Result of %d! is %d", n, result);
     return 0;
 }
