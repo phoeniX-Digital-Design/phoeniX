05a07b909d01635b1466c89b61a611eddb490cdb
ArvinDelavari <arvin7807@gmail.com>
2024-01-12 12:08:18 +0330

    ✅ Run and execute sum1ton on phoeniX

--- Software/Sample_C_Codes/sum1ton/sum1ton.c
+++ Software/Sample_C_Codes/sum1ton/sum1ton.c
@@ -3,11 +3,12 @@
 int main()
 {
     int sum = 0;
-    int n = 10;
+    int n = 100;
     
     for (int i = 1 ; i <= n ; ++i)
     {
         sum += i;
     }
+    printf("Sum of 1 to %d is %d!", n, sum);
     return 0;
 }
