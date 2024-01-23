298888430ad334c105ae7616a1604b5dc1168562
ArvinDelavari <arvin7807@gmail.com>
2024-01-23 20:03:22 +0330

    🦺 Create a sample divison test code

---
+++ Software/Sample_Assembly_Codes/division/division.s
@@ -0,0 +1,10 @@
+addi    x1,    x0,    0x07f9
+
+addi    x12,    x0,    2047
+addi    x13,    x0,    10
+addi    x14,    x0,    10
+
+div     x15,    x12,   x13
+remu     x16,    x15,   x14
+
+ebreak
